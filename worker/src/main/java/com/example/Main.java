a58cde9c6288a923d5ad288d3331b8c5600a5ef3
Fabian Zamudio <fabian.zamudio.89@gmail.com>
2023-01-26 11:15:44 -0300

    Add Procfile

--- worker/src/main/java/com/example/Main.java
+++ worker/src/main/java/com/example/Main.java
@@ -1,9 +1,10 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         var tester = new Tester();
         tester.showMessage();
-
     }
 }
--- worker/src/main/java/com/example/Tester.java
+++ worker/src/main/java/com/example/Tester.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import static java.lang.Thread.sleep;
+
 public class Tester {
 
     public void showMessage() throws InterruptedException {
         while(true){
-            wait(1000);
+            sleep(1000);
             System.out.println("Hello from worker");
         }
     }
